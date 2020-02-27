ac1302ef8073bcacd12c56be73beadd2b1d2f7ec
jianqing <jianqing@luojilab.com>
2020-02-27 23:30:36 +0800

    命令的使用,以及颜色的使用

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import commander from 'commander';
+import colors from 'colors';
 
+const command = commander
+    .version("0.1.0")
+    .option("-c, --city [name]", "add city name")
+    .parse(process.argv);
+   
+    if (process.argv.slice(2).length === 0){
+      command.outputHelp(colors.red);
+      process.exit();
+    }
+    console.log(command.city);
